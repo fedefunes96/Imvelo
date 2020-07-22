395a2891f9d5f54b408219d87203eb52b9526d05
azowyl <facugerstner_29@hotmail.com>
2020-07-22 13:44:56 -0300

    add cache logic

--- app/screens/home_screen.js
+++ app/screens/home_screen.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import VideoList from './VideoList';
+import convertToProxyURL from 'react-native-video-cache';
 
-const HomeScreen = ({navigation}) => (
-  <VideoList videos={require('../../assets/videos')} navigation={navigation} />
-);
+const HomeScreen = ({navigation}) => {
+  const [videos, setVideos] = useState(require('../../assets/videos'));
+
+  useEffect(() => {
+    const cacheVideos = async () => {
+      const cachedVideos = await Promise.all(
+        videos.map(async video => {
+          const newSource = await convertToProxyURL(video.source);
+          const newThumb = await convertToProxyURL(video.thumb);
+          return {...video, source: newSource, thumb: newThumb};
+        }),
+      );
+      setVideos(cachedVideos);
+    };
+    cacheVideos();
+  }, []);
+
+  return <VideoList videos={videos} navigation={navigation} />;
+};
 
 export default HomeScreen;
