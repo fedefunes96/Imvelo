62814756632ce58225a773d3d66e511a83c78cc8
azowyl <facugerstner_29@hotmail.com>
2020-07-22 13:47:37 -0300

    add scrollview to info

--- app/screens/video_info.js
+++ app/screens/video_info.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import {styles} from '../styles/common';
 
 const VideoInfo = props => (
   <View style={[styles.video_tile, styles.video_info]}>
-    <Text style={styles.color_w}>{props.video.description}</Text>
+    <ScrollView nestedScrollEnabled={true}>
+      <Text style={styles.color_w}>{props.video.description}</Text>
+    </ScrollView>
   </View>
 );
 
