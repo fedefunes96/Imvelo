2271542e72ff95c3d3c221f2660d8827ccd38143
azowyl <facugerstner_29@hotmail.com>
2020-07-22 14:00:56 -0300

    add fetching logic

--- app/screens/home_screen.js
+++ app/screens/home_screen.js
@@ -2,13 +2,17 @@ import React, {useEffect, useState} from 'react';
 import VideoList from './VideoList';
 import convertToProxyURL from 'react-native-video-cache';
 
+const VIDEOS_URL = "https://firebasestorage.googleapis.com/v0/b/imvelo-ac4e9.appspot.com/o/videos.json?alt=media&token=8050a750-9eb5-47c9-96e1-b90ab9baa4c3";
+
 const HomeScreen = ({navigation}) => {
-  const [videos, setVideos] = useState(require('../../assets/videos'));
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    const cacheVideos = async () => {
+    const fetchVideos = async () => {
+      const response = await fetch(VIDEOS_URL);
+      const rawVideos = await response.json();
       const cachedVideos = await Promise.all(
-        videos.map(async video => {
+        rawVideos.map(async video => {
           const newSource = await convertToProxyURL(video.source);
           const newThumb = await convertToProxyURL(video.thumb);
           return {...video, source: newSource, thumb: newThumb};
@@ -16,7 +20,7 @@ const HomeScreen = ({navigation}) => {
       );
       setVideos(cachedVideos);
     };
-    cacheVideos();
+    fetchVideos();
   }, []);
 
   return <VideoList videos={videos} navigation={navigation} />;
